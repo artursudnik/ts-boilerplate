0cdae04b39d61217eb56a97333909a07ef822582
Artur Sudnik-Hrynkiewicz <artur.sudnik@gmail.com>
2024-11-12 16:31:37 +0100

    chore: changing eslint to not be executed on .js files

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   '*.{md,json,yaml,yml}': [`prettier -w`],
   '*.ts': ['organize-imports-cli', `prettier -w`, 'eslint'],
-  '*.js': [`prettier -w`, 'eslint'],
+  '*.js': [`prettier -w`],
 };
